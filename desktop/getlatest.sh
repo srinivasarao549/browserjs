e6834befde6302e881fc0834f6bf0d19100c97e7
Ola P. Kleiven <olak@opera.com>
2012-01-13 16:29:16 +0100

    ignore

--- desktop/getlatest.sh
+++
@@ -1,9 +0,0 @@
-FILEPATH="~/public_html/browserjs/signed/"
-FILE="browserjs-desktop-"
-VER=$1
-DATE=$2
-scp olak@homes:~/public_html/browserjs/signed/browserjs-desktop-$VER-$DATE.js.gz .
-echo "fetched $FILE$VER-$DATE.js.gz"
-gunzip $FILE$VER-$DATE.js.gz
-mv $FILE$VER-$DATE.js browserjs-$VER.js
-echo "Done"
